75ceea7b0215c9edb1b90430eba6255f2d00caaf
Isidoros <tsa.isidoros@gmail.com>
2022-05-30 14:25:04 +0300

    km, kt

--- matlab/w_theta_vtacho_u_time.m
+++ matlab/w_theta_vtacho_u_time.m
@@ -6,7 +6,7 @@ function [w theta v_tacho u time] = vtacho_u_time(a, u_value)
     % rpm -> rad/s
     Ko = 2*pi/60;
 
-    % 
+    % Ki (theta -> volt before reduction by 1/3)
     Ki = 12.5/(2*pi);
 
     % calculating manually TODO
@@ -24,9 +24,22 @@ function [w theta v_tacho u time] = vtacho_u_time(a, u_value)
     while(iter < 100)
         iter = iter+1;
 
-        theta_curr = 3 * readVoltage(a, 'A5') / Ki;
-        v_tacho_curr = 2*(2*readVoltage, 'A3') - V_7805);
-        w_curr = 
+        % in rads
+        theta_curr = 3*readVoltage(a, 'A5')/Ki;
+
+        % volts
+        v_tacho_curr = 2*((2*readVoltage, 'A3')-V_7805);
+
+        % w_curr in rpm 
+        w_curr = v_tacho_curr*(1/Kt) 
+
+        % now in rad/s
+        w_curr = w_curr*Ko
+
+        theta(end+1) = theta_curr;
+        v_tacho(end+1) = v_tacho_curr;
+        w(end+1) = w_curr;
+        u(end+1) = u_value;
 
         time(end+1) = toc;
     end
